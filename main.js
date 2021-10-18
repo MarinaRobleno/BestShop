dcf802997912bc7b6260778bf623c74341333e59
Marina Robleño <marinarsanisidro@gmail.com>
2021-10-18 12:48:35 +0200

    Fixed menu animation

--- main.js
+++ main.js
@@ -3,19 +3,39 @@ const closeButton = document.getElementById("close");
 let dropdownContent = document.getElementById("header-links");
 let isHidden = true;
 
+
+
+let percentage = document.getElementById("percentage");
+percentage.innerHTML = '';
+
+
+//Hamburger menu function
 function openDropdown(){
     if(isHidden === true){
-        dropdownContent.style.display = 'block';
+        dropdownContent.classList.add('header-links--visible');
         openButton.style.display = 'none';
         closeButton.style.display = 'block';
         isHidden = false;
     }else{
-        dropdownContent.style.display = 'none';
+        dropdownContent.classList.remove('header-links--visible');
         openButton.style.display = 'block';
         closeButton.style.display = 'none';
         isHidden = true;
     }
 }
 
+//Black hover function
+function blackHover(){
+    
+}
+
+//Percentage scroll function
+function updatePercentage() {
+
+}
+
 openButton.addEventListener('click', openDropdown);
 closeButton.addEventListener('click',openDropdown);
+
+
+
