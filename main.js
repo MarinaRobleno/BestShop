d8b1024b74558489fdd18e0f4acaa3306b22b9ad
Marina Robleño <marinarsanisidro@gmail.com>
2021-10-18 10:52:10 +0200

    Prepared hamburger dropdown menu, fixed media only screen header bar

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-const openDropdown = document.getElementById("open");
-const closeDropdown = document.getElementById("close");
+const openButton = document.getElementById("open");
+const closeButton = document.getElementById("close");
+const dropdownContent = document.querySelector("header-links");
 
+function openDropdown(){
+    if(dropdownContent.style.display == 'none'){
+        dropdownContent.style.display = 'block';
+        openButton.style.display = 'none';
+        closeButton.style.display = 'block';
+    }
+}
+
+function closeDropdown(){
+
+}
+
+openButton.addEventListener('click', openDropdown);
